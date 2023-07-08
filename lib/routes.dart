3609ccf015e7cd029043150877cc0f6d2a762b80
hlj <1759158360@qq.com>
2023-07-08 16:35:10 +0800

    111

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:mine_platform_app/pages/index_page/index_page.dart';
 import 'package:mine_platform_app/pages/login_page/login_page.dart';
+import 'package:mine_platform_app/pages/index_page/child_pages/mine_page/mine_setting_page.dart';
+
 
 final String splashPath = 'splash';
 final String indexPage = "/";
 final String loginPage = '/login';
+final String mineSettingPage = '/mineSettingPage';
 final String notFoundPath = '404';
 var router = {
   loginPage: (context) => const LoginPage(),
   indexPage: (context) => const IndexPage(),
+  mineSettingPage: (context) => const MineSettingPage(),
 };
 var onGenerateRoute = (RouteSettings settings) {
   final String? name = settings.name;
