7c109ac269a541e90774d7136ff6c11a74a2a8f7
wangyuefeng <276410287@qq.com>
2023-07-11 16:40:41 +0800

    s

--- lib/pages/index_page/child_pages/notify_page/notify_page.dart
+++ lib/pages/index_page/child_pages/notify_page/notify_page.dart
@@ -1,5 +1,6 @@
 import 'package:barcode_scan2/barcode_scan2.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class NotifyPage extends StatelessWidget {
   const NotifyPage({Key? key}) : super(key: key);
@@ -31,10 +32,16 @@ class _BarcodeScanPageState extends State<BarCodeScanPage> {
         strings: {'cancel': '取消', 'flash_on': '闪光灯开/关', 'flash_off': '闪光灯开/关'});
 
     var result = await BarcodeScanner.scan(options: options);
-    ShowScanResult(result.type, result.rawContent, result.format);
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(position.latitude.toString());
+    print(position.longitude.toString());
+    ShowScanResult(result.type, result.rawContent, result.format,
+        position.latitude.toString(), position.longitude.toString());
   }
 
-  Future<bool?> ShowScanResult(var type, var content, var format) {
+  Future<bool?> ShowScanResult(
+      var type, var content, var format, var latitude, var longitude) {
     return showDialog(
         context: context,
         builder: (context) {
@@ -45,6 +52,8 @@ class _BarcodeScanPageState extends State<BarCodeScanPage> {
                   Text(type.toString()),
                   Text(content.toString()),
                   Text(format.toString()),
+                  Text(latitude),
+                  Text(longitude),
                 ],
               ));
         });
